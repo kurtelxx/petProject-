a8decd2a5b77cd25e965f1a36c8cfa33e1bef767
kurtelxx <ker4_01@mail.ru>
2023-11-29 07:33:28 +0300

    preloader + refactor mapdispatch

---
+++ src/Components/Common/Preloader/Preloader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import loader from '../../../assets/images/Preloader.gif'
+
+
+
+let Preloader = (props) => {
+    return <div>
+        <img src={loader}/>
+    </div>
+}
+
+
+export default Preloader

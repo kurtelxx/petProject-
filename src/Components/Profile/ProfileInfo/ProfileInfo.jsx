d720dc5619d25f2762c462ad5dc6c54cdcd6afd9
kurtelxx <ker4_01@mail.ru>
2023-12-27 03:21:03 +0300

    photos

--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,24 +3,28 @@ import s from './ProfileInfo.module.css';
 import Preloader from "../../Common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus"
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from "../../../assets/images/img.png";
 
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto }) => {
+if (!profile) {
+    return <Preloader />
+}
 
-const ProfileInfo = (props) => {
-    if (!props.profile) {
-        return <Preloader />
+const onMainPhotoSelected = (e) => {
+    if (e.target.files.length) {
+        savePhoto(e.target.files[0]);
     }
-    return (
-        <div>
-            <div>
-                <img className={s.kotik}
-                     src='https://sun9-15.userapi.com/impg/DadLoejAur6Ym91JxKqnsP8YNyjUGlknoDHq1g/WC0WVzW_hvo.jpg?size=296x376&quality=96&sign=df585aeb83bb7944ef952edd02db5f40&type=album'/>
-            </div>
-            <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large} />
-                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
-            </div>
+}
+
+return (
+    <div>
+        <div className={s.descriptionBlock}>
+            <img src={profile.photos.large || userPhoto} className={s.mainPhoto} />
+            { isOwner && <input type={"file"} onChange={onMainPhotoSelected} />}
+            <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
         </div>
-    )
+    </div>
+)
 }
 
 export default ProfileInfo;
